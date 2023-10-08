ff2c861116f030624fbb9ed45ea92f9929cb8354
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2023-10-08 09:06:15 +0200

    Backend - Creation of the “User” entity

---
+++ backend-spring-boot/src/main/java/com/dgmf/entity/User.java
@@ -0,0 +1,42 @@
+package com.dgmf.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+import org.hibernate.annotations.UpdateTimestamp;
+import java.time.LocalDateTime;
+
+@Entity
+@Data @NoArgsConstructor @AllArgsConstructor @Builder
+// @DynamicInsert @DynamicUpdate ==> Deprecated since Spring 6
+@Table(name = "tbl_user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+    @Column(name = "first_name", nullable = false, length = 20)
+    private String firstName;
+    @Column(name = "last_name", nullable = false, length = 20)
+    private String lastName;
+    @Column(name = "contact_number", nullable = false, length = 20)
+    private String contactNumber;
+    @Column(nullable = false, length = 50)
+    private String email;
+    private String password;
+    private String status;
+    private String role;
+    @Column(name = "date_created", nullable = false)
+    // Hibernate will automatically take the current Timestamp of the JVM
+    @CreationTimestamp
+    private LocalDateTime dateCreated;
+    @Column(name = "last_updated")
+    // Hibernate will automatically take the current Timestamp of the JVM
+    @UpdateTimestamp
+    private LocalDateTime lastUpdated;
+}
